269b6b313fdd6a9c187fa21c7e4c986c28847a73
sadik <po480373@gmai.com>
2022-03-01 01:33:38 +0600

    search functionality added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('good to go')
+// https://openapi.programming-hero.com/api/phones?search=${searchText}
+const searchBox = () => {
+  const searchBox = document.getElementById("search-box").value;
+  loadPhones(searchBox.toLowerCase());
+};
+const loadPhones = (searchText) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
+};

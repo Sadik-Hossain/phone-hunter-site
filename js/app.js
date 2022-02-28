b1576e4cb5b59b84c02e04a68bc696363771f94f
sadik <po480373@gmai.com>
2022-03-01 02:08:55 +0600

    search result card  added

--- js/app.js
+++ js/app.js
@@ -3,9 +3,34 @@ const searchBox = () => {
   const searchBox = document.getElementById("search-box").value;
   loadPhones(searchBox.toLowerCase());
 };
+//------------------Fetch Data------------------------------
 const loadPhones = (searchText) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
+    .then((data) => showPhones(data.data));
+};
+//-------------------Search Result---------------------------- 
+const showPhones = (phones) => {
+  const showDiv = document.getElementById("show");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card h-100 mx-auto text-center" style="width: 18rem">
+            <img
+              class="w-50 mx-auto p-3"
+              src="${phone.image}"
+              alt="..."
+            />
+            <div class="card-body">
+              <h4 class="card-title">${phone.phone_name}</h4>
+              <p class="card-text">brand: ${phone.brand}</p>
+              <a href="#" class="btn btn-primary">Details</a>
+            </div>
+          </div>
+    `;
+    showDiv.appendChild(div);
+  });
 };

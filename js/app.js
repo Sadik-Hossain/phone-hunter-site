13c3cdbe18fef61d522f78237800fc883adbf683
sadik <po480373@gmai.com>
2022-03-01 15:28:15 +0600

    loading and scrollbar added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 // https://openapi.programming-hero.com/api/phones?search=${searchText}
+const toggleSpinner = (displayStyle) => {
+  document.getElementById("spinner").style.display =displayStyle;
+};
 const searchBox = () => {
   const searchBox = document.getElementById("search-box").value;
+  toggleSpinner('block');
   loadPhones(searchBox.toLowerCase());
+  console.log(searchBox);
+  document.getElementById("search-box").value = "";
 };
 //------------------Fetch Data------------------------------
 const loadPhones = (searchText) => {
@@ -13,10 +19,21 @@ const loadPhones = (searchText) => {
 //-------------------Search Result----------------------------
 const showPhones = (phones) => {
   const showDiv = document.getElementById("show");
-  phones.forEach((phone) => {
+  showDiv.textContent = "";
+  console.log(phones.length);
+  console.log(phones.slice(0, 20));
+  console.log(phones.slice(0));
+  const first20Res = phones.slice(0, 20);
+  const allRes = phones.slice(0);
+  if (phones.length === 0) {
+    showDiv.innerHTML = `<p class = "text-danger text-center mx-auto">Sorry nothing found</p>`;
+  }
+  first20Res.forEach((phone) => {
+    // showAll(phone);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
+   
     <div class="card h-100 mx-auto text-center" style="width: 18rem">
             <img
               class="w-50 mx-auto p-3"
@@ -26,12 +43,17 @@ const showPhones = (phones) => {
             <div class="card-body">
               <h4 class="card-title">${phone.phone_name}</h4>
               <p class="card-text">brand: ${phone.brand}</p>
-              <button onclick='getId("${phone.slug}")' class="btn btn-primary">Details</button>
+              <button onclick='getId("${phone.slug}")' class="btn btn-primary" >
+              <a class="nav-link text-white" href="#header">Details</a>
+              
+               </button>
             </div>
           </div>
     `;
     showDiv.appendChild(div);
+    toggleSpinner('none ');
   });
+  document.getElementById("show-all-btn").style.display = "block";
 };
 // -----------Phone ID------------------------------
 // https://openapi.programming-hero.com/api/phone/${id}
@@ -41,12 +63,14 @@ const getId = (PhoneIds) => {
     .then((res) => res.json())
     .then((data) => showDetails(data.data));
 };
+//---------------Detail section---------------------------
 const showDetails = (phoneDetails) => {
   console.log(phoneDetails.image);
   const detailsDiv = document.getElementById("detail");
-
+  detailsDiv.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
+  <h3 class='text-center'>Product Details</h3>
   <div class="container border my-3">
     <div class="d-flex flex-row">
         <div class=" flex-fill p-3">
@@ -79,6 +103,47 @@ const showDetails = (phoneDetails) => {
         </div>
     </div>
   </div>
+ 
  `;
   detailsDiv.appendChild(div);
 };
+
+//-------------ShowAll------------------------------------
+/* const showAll = (phone) => {
+  const showDiv = document.getElementById("show");
+  // showDiv.textContent = "";
+  const div = document.createElement("div");
+  div.classList.add("col");
+  div.innerHTML = `
+    <div class="card h-100 mx-auto text-center" style="width: 18rem">
+            <img
+              class="w-50 mx-auto p-3"
+              src="${phone.image}"
+              alt="..."
+            />
+            <div class="card-body">
+              <h4 class="card-title">${phone.phone_name}</h4>
+              <p class="card-text">brand: ${phone.brand}</p>
+              <button onclick='getId("${phone.slug}")' class="btn btn-primary" >
+              Details
+               </button>
+            </div>
+          </div>
+    `;
+  showDiv.appendChild(div);
+  document.getElementById("show-all-btn").style.display = "block";
+}; */
+
+// ------------------Scrolling Effect Function-----------------
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}

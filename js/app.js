e28205e05ac26cf07407c72f8afee2ec941a6f46
sadik <po480373@gmai.com>
2022-03-01 21:17:49 +0600

    spinner fix and draft detail

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 // https://openapi.programming-hero.com/api/phones?search=${searchText}
+//-----------Spinner & hide result----------------------------
 const toggleSpinner = (displayStyle) => {
-  document.getElementById("spinner").style.display =displayStyle;
+  document.getElementById("spinner").style.display = displayStyle;
 };
+const toggleSearchResult = (displayStyle) => {
+  document.getElementById("toggle-show").style.display = displayStyle;
+};
+const toggleDetail = (displayStyle) => {
+  document.getElementById("toggle-detail").style.display = displayStyle;
+};
+//-------------------getting Input Value------------------------
 const searchBox = () => {
   const searchBox = document.getElementById("search-box").value;
-  toggleSpinner('block');
+  toggleSpinner("block");
+  toggleSearchResult("none");
   loadPhones(searchBox.toLowerCase());
   console.log(searchBox);
   document.getElementById("search-box").value = "";
+  document.getElementById("detail").textContent = "";
 };
 //------------------Fetch Data------------------------------
 const loadPhones = (searchText) => {
@@ -18,46 +28,53 @@ const loadPhones = (searchText) => {
 };
 //-------------------Search Result----------------------------
 const showPhones = (phones) => {
+
   const showDiv = document.getElementById("show");
   showDiv.textContent = "";
   console.log(phones.length);
   console.log(phones.slice(0, 20));
-  console.log(phones.slice(0));
+
   const first20Res = phones.slice(0, 20);
-  const allRes = phones.slice(0);
+
   if (phones.length === 0) {
     showDiv.innerHTML = `<p class = "text-danger text-center mx-auto">Sorry nothing found</p>`;
-  }
-  first20Res.forEach((phone) => {
-    // showAll(phone);
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-   
-    <div class="card h-100 mx-auto text-center" style="width: 18rem">
+    document.getElementById("show-all-btn").style.display = "none";
+  } else {
+    first20Res.forEach((phone) => {
+      const div = document.createElement("div");
+      div.classList.add("col");
+
+      div.innerHTML = `
+      
+    <div class=" card border-dark h-100 mx-auto text-center bg-color-1 " style="width: 18rem">
             <img
               class="w-50 mx-auto p-3"
               src="${phone.image}"
               alt="..."
             />
-            <div class="card-body">
+            <div class="card-body ">
               <h4 class="card-title">${phone.phone_name}</h4>
               <p class="card-text">brand: ${phone.brand}</p>
-              <button onclick='getId("${phone.slug}")' class="btn btn-primary" >
+              <button onclick='getId("${phone.slug}")' class="btn btn-dark" >
               <a class="nav-link text-white" href="#header">Details</a>
               
                </button>
             </div>
           </div>
     `;
-    showDiv.appendChild(div);
-    toggleSpinner('none ');
-  });
-  document.getElementById("show-all-btn").style.display = "block";
+      showDiv.appendChild(div);
+      document.getElementById("show-all-btn").style.display = "block";
+    });
+  }
+  toggleSpinner("none");
+  toggleSearchResult("block");
 };
 // -----------Phone ID------------------------------
 // https://openapi.programming-hero.com/api/phone/${id}
 const getId = (PhoneIds) => {
+  toggleSpinner("block");
+  toggleDetail("none");
+
   const url = `https://openapi.programming-hero.com/api/phone/${PhoneIds}`;
   fetch(url)
     .then((res) => res.json())
@@ -65,13 +82,17 @@ const getId = (PhoneIds) => {
 };
 //---------------Detail section---------------------------
 const showDetails = (phoneDetails) => {
-  console.log(phoneDetails.image);
+  console.log(phoneDetails.mainFeatures.sensors);
+  phoneDetails.mainFeatures.sensors.forEach(element => {
+    console.log(element)
+  });
+    //mainFeatures.sensors
   const detailsDiv = document.getElementById("detail");
   detailsDiv.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
+  <div class="container border-dark my-3">
   <h3 class='text-center'>Product Details</h3>
-  <div class="container border my-3">
     <div class="d-flex flex-row">
         <div class=" flex-fill p-3">
             <img
@@ -101,38 +122,20 @@ const showDetails = (phoneDetails) => {
               phoneDetails.mainFeatures.memory
             }</p>
         </div>
+        
     </div>
   </div>
  
  `;
+ const sensorDiv = document.createElement('div')
+sensorDiv.innerHTML=`
+<h3 class='text-center'>Sensor</h3>
+`
   detailsDiv.appendChild(div);
+  detailsDiv.appendChild(sensorDiv);
+  toggleSpinner("none");
+  toggleDetail("block");
 };
-
-//-------------ShowAll------------------------------------
-/* const showAll = (phone) => {
-  const showDiv = document.getElementById("show");
-  // showDiv.textContent = "";
-  const div = document.createElement("div");
-  div.classList.add("col");
-  div.innerHTML = `
-    <div class="card h-100 mx-auto text-center" style="width: 18rem">
-            <img
-              class="w-50 mx-auto p-3"
-              src="${phone.image}"
-              alt="..."
-            />
-            <div class="card-body">
-              <h4 class="card-title">${phone.phone_name}</h4>
-              <p class="card-text">brand: ${phone.brand}</p>
-              <button onclick='getId("${phone.slug}")' class="btn btn-primary" >
-              Details
-               </button>
-            </div>
-          </div>
-    `;
-  showDiv.appendChild(div);
-  document.getElementById("show-all-btn").style.display = "block";
-}; */
 
 // ------------------Scrolling Effect Function-----------------
 window.onscroll = function () {
